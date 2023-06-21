4b1c96aed9caa062deece125f6514838606f3aab
Varun Singh <varuns@propertyguru.tech>
2023-06-21 08:35:04 +0530

    mfe setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'container';
+  @ViewChild('childComponent', { read: ViewContainerRef }) viewContainer!: ViewContainerRef;
+  component: any;
+  childComponentLoaded = false;
+
+  ngOnInit() {
+    this.loadFrontend1Component();
+  }
+
+  loadFrontend1Component() {
+    import('mfe1/Component').then(m => {
+      console.log(m.AppComponent);
+      this.component = m.AppComponent;
+    })
+
+
+  }
+
+  toggle() {
+    this.childComponentLoaded = !this.childComponentLoaded;
+    setTimeout(() => {
+      if(this.childComponentLoaded && this.component) {
+        const componentRef = this.viewContainer.createComponent(this.component);
+        (componentRef.instance as any).appControllerName = 'container app';
+      }
+    })
+
+
+  }
 }
